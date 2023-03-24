b7435b40be66c13b38c51861d0a3ef097a423e91
davidknight <davidb_nunes@yahoo.com.br>
2023-03-23 20:09:31 -0500

    Functions Continued

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function saturdayFun() {  
-    console.log(`this Saturday, I want to ${"roller-skate"}!`)    
+function saturdayFun(activity = "roller-skate") {  
+    return `This Saturday, I want to ${activity}!`;    
 }
-saturdayFun;
+console.log(saturdayFun());
 
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork("This Saturday, I want to bathe my dog!"));
+
+function wrapAdjective(symbol = "*") {
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    }
+
+}
